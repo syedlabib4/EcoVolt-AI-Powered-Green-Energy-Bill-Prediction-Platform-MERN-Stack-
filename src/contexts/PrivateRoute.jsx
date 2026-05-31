2cb637f983beb3fcdc2efbae111d3db8f097462d
SYED LABIB <124432659+syedlabib4@users.noreply.github.com>
2026-05-31 14:42:30 +0500

    Delete src directory

--- src/contexts/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "./AuthContext";
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
